3b822cb70daf128990041e64e69f20ee631c52c9
lixiaohui <lixiaohui.1024@bytedance.com>
2020-10-09 09:56:50 +0800

    assert: add more functions

--- assert/assert.go
+++ assert/assert.go
@@ -1,5 +1,33 @@
 package assert
 
-func Equal[T comparable](left, right T) {
+import "fmt"
 
+func Assert(expr bool, msg string) {
+	if !expr {
+		panic(msg)
+	}
+}
+
+func Assertf(expr bool, format string, args ...interface{}) {
+	Assert(expr, fmt.Sprintf(format, args...))
+}
+
+func Equal[T comparable](left, right T, msg string) {
+	if left != right {
+		panic(msg)
+	}
+}
+
+func Equalf[T comparable](left, right T, format string, args ...interface{}) {
+	Equal(left, right, fmt.Sprintf(format, args...))
+}
+
+func NotEqual[T comparable](left, right T, msg string) {
+	if left == right {
+		panic(msg)
+	}
+}
+
+func NotEqualf[T comparable](left, right T, format string, args ...interface{}) {
+	NotEqual(left, right, fmt.Sprintf(format, args...))
 }

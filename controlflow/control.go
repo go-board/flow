012a81e1b1192ea620dc859595d4a1eca1db96e5
lixiaohui <lixiaohui.1024@bytedance.com>
2020-10-09 16:22:12 +0800

    [controlflow] add Condition[al]Expr

--- controlflow/control.go
+++ controlflow/control.go
@@ -1,12 +1,46 @@
 package controlflow
 
+type Condition[T any] bool
+
+func New[T any](expr bool) Condition[T] {
+	return Condition[T](expr)
+}
+
+func (cond Condition[T]) Return(left, right T) T {
+	if bool(cond) {
+		return left
+	}
+	return right
+}
+
+type ConditionExpr[T any] func() bool
+
+func NewExpr[T any](exprFn func() bool) ConditionExpr[T] {
+	return ConditionExpr[T](exprFn)
+}
+
+func (cond ConditionExpr[T]) Return(left, right T) T {
+	if cond() {
+		return left
+	}
+	return right
+}
+
 // Conditional return left if expression is ture, else return right.
 //  for example:
 // 		a > b ? a : b
 //		strings.Contains("a", "b") ? 3 : 1
-func Conditional[T any](expression bool, left T, right T) T {
+func Conditional[T any](expression bool, left, right T) T {
 	if expression {
 		return left
 	}
 	return right
+}
+
+func ConditionalExpr(expression bool, left func (), right func()) {
+	if expression {
+		left()
+	} else {
+		right()
+	}
 }
